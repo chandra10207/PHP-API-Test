115e8d716e71c13b59041272e18138dcdbc7a88f
Chandra Pandey <chandra.pandey@MacBook-Pro.local>
2022-03-22 17:08:54 +1100

    test response message added on all products

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -11,6 +11,7 @@ class ApiController extends Controller
         $all_products_path = public_path() . "/assets/products/all_products.json";
         $all_products = $all_products = file_get_contents($all_products_path);
         $product_json = json_decode($all_products);
+        // Some test data here
         return response($product_json, 200);
     }
 
